ab40342fb5a4006b41f2469621ff8b0b0271b8fd
Benjamin <benjamin@payflows.io>
2025-07-27 11:39:38 +0200

    feat(StudentSettings): change api folder name

--- server/api/grade/index.get.ts
+++
@@ -1,9 +0,0 @@
-import { defineEventHandler } from 'h3';
-
-export default defineEventHandler(() => {
-  const grades = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
-  const randomIndex = Math.floor(Math.random() * grades.length);
-  return {
-    grade: grades[randomIndex],
-  };
-});
